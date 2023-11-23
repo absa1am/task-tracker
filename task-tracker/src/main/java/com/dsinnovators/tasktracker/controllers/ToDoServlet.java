d2aa84d92f0ed5ece72cc58bba03b06e1a00b802
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-11-23 11:28:16 +0600

    added ToDoServlet

---
+++ task-tracker/src/main/java/com/dsinnovators/tasktracker/controllers/ToDoServlet.java
@@ -0,0 +1,19 @@
+package com.dsinnovators.tasktracker.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ToDoServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("todo.jsp");
+
+        requestDispatcher.forward(request, response);
+    }
+
+}
