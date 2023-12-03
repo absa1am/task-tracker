017f863a41b838ca190abecf7ac49e663809098c
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-12-03 18:05:06 +0600

    added database.properties for database config

--- task-tracker/src/main/java/com/dsinnovators/tasktracker/dao/DAO.java
+++ task-tracker/src/main/java/com/dsinnovators/tasktracker/dao/DAO.java
@@ -1,16 +1,29 @@
 package com.dsinnovators.tasktracker.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DAO {
-    private String url = "jdbc:postgresql://localhost:5432/jdbc";
-    private String username = "postgres";
-    private String password = "123456";
 
-    public Connection getConnection() throws SQLException, ClassNotFoundException {
-        Class.forName("org.postgresql.Driver");
+    public Connection getConnection() throws SQLException, ClassNotFoundException, IOException {
+        Properties properties = new Properties();
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("database.properties");
+
+        properties.load(inputStream);
+
+        // retrieving database config
+        String driver = properties.getProperty("database.driver");
+        String url = properties.getProperty("database.url");
+        String username = properties.getProperty("database.username");
+        String password = properties.getProperty("database.password");
+
+        // ensuring driver loaded
+        Class.forName(driver);
 
         Connection connection = DriverManager.getConnection(url, username, password);
         
