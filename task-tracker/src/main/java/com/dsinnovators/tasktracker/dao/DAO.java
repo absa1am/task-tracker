70768feebc0a16f4711643be760ad909deaa5e34
Md. Abdus Salam <salaam.mbstu@gmail.com>
2023-12-04 11:22:24 +0600

    converted DAO class to abstract, so that no-one can create object of DAO but can extends it to use

--- task-tracker/src/main/java/com/dsinnovators/tasktracker/dao/DAO.java
+++ task-tracker/src/main/java/com/dsinnovators/tasktracker/dao/DAO.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class DAO {
+public abstract class DAO {
 
     public Connection getConnection() throws SQLException, ClassNotFoundException, IOException {
         Properties properties = new Properties();
